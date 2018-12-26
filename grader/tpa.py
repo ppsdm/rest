8ab21368a048d20881d3f25eb903a00fe2ea183e
Reno Wijoyo <renowijoyo@gmail.com>
2018-12-26 14:19:39 +0700

    tpa & summary

---
+++ grader/tpa.py
@@ -0,0 +1,91 @@
+from flask import g
+import scale
+import pymysql
+
+
+
+def grader(itemResult):
+    subtest_name = itemResult.attrib['identifier'].split('_')[1].lower()
+    print('entering TPA module : ' + itemResult.attrib['identifier'])
+
+    if 'tpa_total' not in g:
+        g.tpa_total = 0
+
+    if 'tpa_max_score' not in g:
+        g.cfit_max_score = 0
+
+    if 'tpa_correct' not in g:
+        g.tpa_correct = 0
+
+    if 'tpa_incorrect' not in g:
+        g.tpa_incorrect = 0
+
+    if 'tpa_empty' not in g:
+        g.tpa_empty = 0
+
+    if 'subtest_score' not in g:
+        g.subtest_score = {}
+
+    if g.reset_object == 1:
+        g.tpa_correct = 0
+        g.tpa_incorrect = 0
+        g.tpa_empty = 0
+        g.reset_object = 0
+        g.subtest_score = {}
+        g.subtest_score['verbal'] = 0
+        g.subtest_score['kuantitatif'] = 0
+        g.subtest_score['penalaran'] = 0
+    
+    itemGrade = {}
+    score = 0
+    max_score = 0
+    response = None
+
+    for subelem in itemResult:
+        #print(subelem.tag)
+        sub_identifier = subelem.attrib['identifier']
+        sub_split = sub_identifier.split('_')
+        if subelem.attrib['identifier'] == 'SCORE' :
+            for subelem2 in subelem:
+                score = int(subelem2.text)
+                #print(sub_identifier, ' : ' ,score)
+                g.tpa_correct += score
+                if subtest_name in g.subtest_score:
+                    g.subtest_score[subtest_name] += score
+                else:
+                    g.subtest_score[subtest_name] = score
+                #itemGrade[itemResult.attrib['identifier']] = score
+        elif subelem.attrib['identifier'] == 'MAXSCORE' :
+            for subelem2 in subelem:
+                max_score = int(subelem2.text)
+                g.tpa_max_score += max_score
+        elif sub_split[0] == 'RESPONSE' :
+            for subelem2 in subelem:
+                for subelem3 in subelem2:
+                    response = subelem3.text
+                    #print(response)
+                    if response == None :
+                        g.tpa_empty += 1
+                    itemGrade["candidate_response"] = response
+
+
+    g.tpa_incorrect = 50 - g.tpa_correct - g.tpa_empty
+    data = {}
+    data["type"] = 'tpa'
+    data["scores"] = {}
+
+    g.subtest_score['verbal'] = g.subtest_score['subtest1'] + g.subtest_score['subtest2'] + g.subtest_score['subtest3']
+    g.subtest_score['kuantitatif'] = g.subtest_score['subtest4'] + g.subtest_score['subtest6'] + g.subtest_score['subtest5']
+    g.subtest_score['penalaran'] = g.subtest_score['subtest7'] + g.subtest_score['subtest9'] + g.subtest_score['subtest8']
+    #data[subtest_name] = g.tpa_correct
+    #data["scores"]["scaled-6"] = scale.scale('cfit-to-6', g.cfit_correct)
+    #data["scores"]["scaled-20"] = scale.scale('cfit-to-20', g.cfit_correct)
+    #data["scores"]["total"] = g.apm_total
+    #data["scores"]["max_score"] = g.apm_max_score
+    data["answers"] = {}
+    data["answers"]["correct"] = g.tpa_correct
+    data["answers"]["incorrect"] = g.tpa_incorrect
+    data["answers"]["empty"] = g.tpa_empty
+    data["items"] = g.subtest_score
+    #print("exiting TPA module")
+    return data
--- restapi.py
+++ restapi.py
@@ -3,7 +3,7 @@ import grader.disc as disc
 import grader.cfit as cfit
 import grader.papi as papi
 import grader.adkudag4 as adkudag4
-import grader.gatb4 as gatb4, grader.tese as tese, grader.tkdinfo as tkdinfo, grader.tkdanalogiverbal as tkdanalogiverbal, grader.tkdidiot as tkdidiot
+import grader.gatb4 as gatb4, grader.tese as tese, grader.tkdinfo as tkdinfo, grader.tkdanalogiverbal as tkdanalogiverbal, grader.tkdidiot as tkdidiot, grader.tpa as tpa
 import grader.compre as compre
 import os.path
 import requests
@@ -11,6 +11,8 @@ import xml.etree.ElementTree as ET
 from flask import Flask, jsonify, render_template, send_from_directory
 from flask_restplus import Resource, Api
 from config import settings
+import pymysql
+from flask import g
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,6 +21,7 @@ namespaces = {'owl': 'http://www.imsglobal.org/xsd/imsqti_result_v2p1'}
 
 SERVER_URL = 'http://cat.ppsdm.com/'
 BASE_URI = 'http://cat.ppsdm.com%2Fppsdm_cat.rdf%23'
+BASE_URI_MYSQL = 'http://cat.ppsdm.com/ppsdm_cat.rdf#'
 
 GET_LATEST_URI = 'taoResultServer/QtiRestResults/getLatest?'
 GET_RESULT_URI = 'taoResultServer/QtiRestResults/getQtiResultXml?'
@@ -35,35 +38,54 @@ def assessmentResultParser(result) :
     scores = {}
     answers = {}
     testItems = {}
+    errors = ""
 
     items = {}
 
     # Parse assessment result
     assessmentResult = ET.ElementTree(ET.fromstring(result.text))
+
     for itemResult in assessmentResult.iterfind('owl:itemResult', namespaces):
-        testItem = {}
+        #testItems.clear()
         answers = {}
         scores = {}
+        errors = ""
+        #identifier = ""
         identifier = itemResult.attrib['identifier']
+        testItems[identifier] = {}
         #print('isinde item result', identifier)
         mod_name = identifier.split('_')[0].lower()
 
-        testItem["id"] = identifier
-        testItem["mod_name"] = mod_name
+        
+        #testItems["mod_name"] = mod_name
 
         try:
             itemGrade = eval(mod_name).grader(itemResult)
-            scores[mod_name] = itemGrade["scores"]
-            answers[mod_name] = itemGrade["answers"]
-            testItem["attributes"] = itemGrade["attributes"]
+            scores = itemGrade["scores"]
+            answers = itemGrade["answers"]
+            testItems[identifier] = itemGrade["items"]
+            #testItems[identifier]["attributes"] = itemGrade["attributes"]
+            #print("success")
+            items[mod_name] = {
+                "scores":scores,
+                "answers":answers,
+                "testItems":testItems[identifier],
+                "type": mod_name,
+                "errors" : errors
+            }
+
         except:
-            testItem["error"] = "no function"
-        items[mod_name] = {
-            "scores":scores,
-            "answers":answers,
-            "testItems":testItems,
-            "type": mod_name 
-        }
+            errors = "invalid"
+            print('no function for ' + identifier)
+
+#        items[mod_name] = {
+ #               "scores":scores,
+  #              "answers":answers,
+   #             "testItems":testItems[identifier],
+    #            "type": mod_name,
+     #           "errors" : errors
+      #  }
+
         #testItems.append(testItem)
     return items
     # Return assessment result
@@ -118,6 +140,42 @@ class Result(Resource):
 
         # Serve assessment result
         return jsonify(data = data)
+
+@api.route('/getsummary/<string:delivery_id>')
+class Summary(Resource):
+    def get(self, delivery_id):
+        # Assessment information
+        data = {}
+        summary_data = {}
+        #data["type"] = "testResult"
+        #data["id"] = delivery_id
+
+        conn = pymysql.connect(host='db.aws.ppsdm.com', port=3306, user='ppsdm', passwd='ppsdm-mysql', db='catdb')
+        retval = None
+        mysql_uri = BASE_URI_MYSQL + delivery_id
+        #print(mysql_uri)
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM results_storage WHERE delivery = %s", (mysql_uri))
+        #print(cur.description)
+        
+        for row in cur:
+            data = {}
+            
+            retval = row[0]
+            print(row[0]) #result
+            result_uri = row[0].split('#')[1]
+            g.reset_object = 1
+            uri = SERVER_URL + GET_RESULT_URI + 'result=' + BASE_URI + result_uri + '&delivery=' + BASE_URI + delivery_id
+            r = requests.get(uri, headers={'Accept': 'application/xml'}, auth=('admin', 'admin123'))
+            data.update(assessmentResultParser(r))
+            summary_data[result_uri] = data
+
+            #print(row[1]) #testtaker
+        #retval = cur.fetchone()
+        cur.close()
+        conn.close()
+
+        return jsonify(data = summary_data)
 
 if __name__ == '__main__':
     app.config.from_object(settings)
