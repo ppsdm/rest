0878d75746aa0eb25b5086f73c91dfbd5a15766e
andy-shi88 <shi88.andy@gmail.com>
2019-01-07 21:12:58 +0800

    basic get user profile query

--- restapi.py
+++ restapi.py
@@ -14,6 +14,7 @@ from config import settings
 import pymysql
 from flask import g
 import json
+import logging
 
 app = Flask(__name__)
 api = Api(app)
@@ -212,6 +213,46 @@ class Summary(Resource):
         conn.close()
 
         return jsonify(data = summary_data)
+
+@api.route('/getUserProfile/<string:user_id>')
+class UserProfile(Resource):
+    def get(self, user_id):
+        # user profile data
+        logging.info('UserProfile.get(%s)', user_id)
+        user = self.getUserProfileById(user_id)
+        return jsonify(data = user)
+
+    def getUserProfileById(self, user_id):
+        logging.info('UserProfile.getUserProfileById(%s)', user_id)
+        conn = pymysql.connect(host='db.aws.ppsdm.com', port=3306, user='ppsdm', passwd='ppsdm-mysql', db='chamilo_ppsdm_db')
+        cur = conn.cursor(pymysql.cursors.DictCursor)
+        logging.info('Querying SQL')
+        # get some user fields + all associated extra_fields + value
+        cur.execute("SELECT user.id, user.firstname, user.lastname, user.username, user.email, user.official_code, \
+             user.phone, user.picture_uri, user.language, efv.value, ef.variable, ef.display_text \
+            FROM user INNER JOIN extra_field_values AS efv \
+             ON user.id = efv.item_id INNER JOIN extra_field AS ef ON ef.id = efv.field_id \
+             WHERE user.id = %s", user_id)
+        i = 0
+        extra_fields = []
+        data = {}
+        for row in cur:
+            if (i == 0):
+                data.update({'id': row['id']})
+                data.update({'firstname': row['firstname']})
+                data.update({'lastname': row['lastname']})
+                data.update({'username': row['username']})
+                data.update({'official_code': row['official_code']})
+                data.update({'email': row['email']})
+                data.update({'phone': row['phone']})
+                data.update({'picture_uri': row['picture_uri']})
+                data.update({'language': row['language']})
+            extra_fields.append({row['variable']: row['value'], 'display_text': row['display_text']})
+            data.update({'extra_fields': extra_fields})
+            i += 1
+        cur.close()
+        conn.close()
+        return data
 
 if __name__ == '__main__':
     app.config.from_object(settings)
