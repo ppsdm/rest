135badd87e3b7c6d0683ddd55d3a210e039c34e3
Reno Wijoyo <renowijoyo@gmail.com>
2019-02-24 19:06:23 +0700

    add disc grader

--- grader/disc.py
+++ grader/disc.py
@@ -37,24 +37,92 @@ def get(self, user_id, c_id):
         print("tool id = ")
         print(tool_id)
         result_uri = getResultId(user_id, tool_id)
-        delivery_uri = getDeliveryId(result_uri)
+        if (result_uri is not None) :
+            delivery_uri = getDeliveryId(result_uri)
 
-        result_id = result_uri.replace(os.getenv('BASE_URI_MYSQL'), "")
-        delivery_id = delivery_uri.replace(os.getenv('BASE_URI_MYSQL'),"")
-        
-        # Retrieve assessment result
-        uri = os.getenv('SERVER_URL') + os.getenv('GET_RESULT_URI') + 'result=' + os.getenv('BASE_URI') + result_id + '&delivery=' + os.getenv('BASE_URI') + delivery_id
-        r = requests.get(uri, headers={'Accept': 'application/xml'}, auth=('admin', 't40@ppsdm'))
-        # Update data from parsed assessment result
-        #print(uri)
-        #data.update(assessmentResultParser(r,tool_id))
-        #data.update(regulerGrader(data))
-        #data.update(papiParser(data))
-        #return r
-        # Serve assessment result
+            result_id = result_uri.replace(os.getenv('BASE_URI_MYSQL'), "")
+            delivery_id = delivery_uri.replace(os.getenv('BASE_URI_MYSQL'),"")
+            
+            # Retrieve assessment result
+            uri = os.getenv('SERVER_URL') + os.getenv('GET_RESULT_URI') + 'result=' + os.getenv('BASE_URI') + result_id + '&delivery=' + os.getenv('BASE_URI') + delivery_id
+            print(uri)
+            r = requests.get(uri, headers={'Accept': 'application/xml'}, auth=('admin', 't40@ppsdm'))
+            # Update data from parsed assessment result
+            #print(uri)
+            data.update(assessmentResultParser(r,tool_id))
+            #data.update(regulerGrader(data))
+            #data.update(papiParser(data))
+            #return r
+            # Serve assessment result
     return jsonify(data = data)
 
 
+
+##
+## @brief Parse given assessment result
+##
+## @param result The assessment result from fetch request
+##
+## @return Object with calculated scores, answers, and testItems
+##
+def assessmentResultParser(result,toolId) :
+    # Create a new object
+    scores = {}
+    answers = {}
+    testItems = {}
+    errors = ""
+
+    items = {}
+    totalQ = {}
+
+    # Parse assessment result
+    assessmentResult = ET.ElementTree(ET.fromstring(result.text))
+
+    for testResult in assessmentResult.iterfind('owl:testResult', namespaces):
+        datestamp = testResult.attrib['datestamp']
+        items['datestamp'] = datestamp
+
+
+    for itemResult in assessmentResult.iterfind('owl:itemResult', namespaces):
+        #testItems.clear()
+        answers = {}
+        scores = {}
+        errors = ""
+        #identifier = ""
+        identifier = itemResult.attrib['identifier']
+        testItems[identifier] = {}
+        #print('isinde item result', identifier)
+        mod_name = identifier.split('_')[0].lower()
+        
+        
+        if mod_name not in totalQ:
+            totalQ[mod_name] = 0
+        #testItems["mod_name"] = mod_name
+
+        try:
+            itemGrade = eval(mod_name).grader(itemResult,totalQ[mod_name])
+            print("identifier = " + identifier)
+            scores = itemGrade["scores"]
+            answers = itemGrade["answers"]
+            if "items" in itemGrade:
+             testItems[identifier] = itemGrade["items"]
+            #testItems[identifier]["attributes"] = itemGrade["attributes"]
+            print("success")
+            items[mod_name] = {
+                "scores":scores,
+                "answers":answers,
+                "testItems":testItems[identifier],
+                "type": mod_name,
+                "errors" : errors
+            }
+
+        except:
+            errors = "invalid"
+            print('!!!NO FUNCTION FOR!!! ' + identifier)
+
+    return items
+
+    
 def getLtiTools(c_id) :
     conn = pymysql.connect(host='db.aws.ppsdm.com', port=3306, user='ppsdm', passwd='ppsdm-mysql', db='chamilo_ppsdm_db')
     #retval = None
--- tool/disc.py
+++ tool/disc.py
@@ -24,54 +24,54 @@ def matching_graphs(disc_score,totalQ):
     spos = 0
     cpos = 0
 
-    dpos = 1 if disc_score['d3'] >= 20 else 0
-    ipos = 1 if disc_score['i3'] >= 20 else 0
-    spos = 1 if disc_score['s3'] >= 20 else 0
-    cpos = 1 if disc_score['c3'] >= 20 else 0
+    dpos = 1 if g.disc_score['d3'] >= 20 else 0
+    ipos = 1 if g.disc_score['i3'] >= 20 else 0
+    spos = 1 if g.disc_score['s3'] >= 20 else 0
+    cpos = 1 if g.disc_score['c3'] >= 20 else 0
 
-    if disc_score['d3'] > disc_score['i3']:
+    if g.disc_score['d3'] > g.disc_score['i3']:
         _di = '>'
-    elif disc_score['d3'] < disc_score['i3']:
+    elif g.disc_score['d3'] < g.disc_score['i3']:
         _di = '<'
     else:
         _di = '='
 
-    if disc_score['d3'] > disc_score['s3']:
+    if g.disc_score['d3'] > g.disc_score['s3']:
         _ds = '>'
-    elif disc_score['d3'] < disc_score['s3']:
+    elif g.disc_score['d3'] < g.disc_score['s3']:
         _ds = '<'
     else:
         _ds = '='
 
-    if disc_score['d3'] > disc_score['c3']:
+    if g.disc_score['d3'] > g.disc_score['c3']:
         _dc = '>'
-    elif disc_score['d3'] < disc_score['c3']:
+    elif g.disc_score['d3'] < g.disc_score['c3']:
         _dc = '<'
     else:
         _dc = '='
 
-    if disc_score['i3'] > disc_score['s3']:
+    if g.disc_score['i3'] > g.disc_score['s3']:
         _is = '>'
-    elif disc_score['i3'] < disc_score['s3']:
+    elif g.disc_score['i3'] < g.disc_score['s3']:
         _is = '<'
     else:
         _is = '='
 
-    if disc_score['i3'] > disc_score['c3']:
+    if g.disc_score['i3'] > g.disc_score['c3']:
         _ic = '>'
-    elif disc_score['i3'] < disc_score['c3']:
+    elif g.disc_score['i3'] < g.disc_score['c3']:
         _ic = '<'
     else:
         _ic = '='
 
-    if disc_score['s3'] > disc_score['c3']:
+    if g.disc_score['s3'] > g.disc_score['c3']:
         _sc = '>'
-    elif disc_score['s3'] < disc_score['c3']:
+    elif g.disc_score['s3'] < g.disc_score['c3']:
         _sc = '<'
     else:
         _sc = '='
 
-    print(disc_score['d3'], ' : ', disc_score['i3'], ' : ', disc_score['s3'], ' : ', disc_score['c3'])
+    print(g.disc_score['d3'], ' : ', g.disc_score['i3'], ' : ', g.disc_score['s3'], ' : ', g.disc_score['c3'])
     print(dpos, ' : ', ipos, ' : ', spos, ' : ', cpos)
     print('di : ', _di, 'ds : ', _ds, 'dc : ', _dc, 'is : ', _is, 'ic : ', _ic, 'sc : ', _sc)
     conn = pymysql.connect(host='db.aws.ppsdm.com', port=3306, user='ppsdm', passwd='ppsdm-mysql', db='atsdb')
@@ -105,47 +105,37 @@ def matching_graphs(disc_score,totalQ):
     return valid, retval
 
 
-def grader(itemResult):
-    if 'apm_total' not in g:
-        g.apm_total = 0
-
-    if 'apm_max_score' not in g:
-        g.apm_max_score = 0
-
-    if 'apm_correct' not in g:
-        g.apm_correct = 0
-
-    if 'apm_incorrect' not in g:
-        g.apm_incorrect = 0
-
-    if 'apm_empty' not in g:
-        g.apm_empty = 0
+def grader(itemResult,totalQ):
 
     itemGrade = {}
-    responses_array = {}
+
+    if 'responses_array' not in g:
+        g.responses_array = {}
+
     score = 0
     max_score = 0
     response = None
     validity = True
     choice = 0
 
-    disc_score = {
-        'd1' : None,
-        'd2' : None,
-        'd3' : None,
-        'i1' : None,
-        'i2' : None,
-        'i3' : None,
-        's1' : None,
-        's2' : None,
-        's3' : None,
-        'c1' : None,
-        'c2' : None,
-        'c3' : None,
-        'disc1' : None,
-        'disc2' : None
+    if 'disc_score' not in g:
+        g.disc_score = {
+            'd1' : None,
+            'd2' : None,
+            'd3' : None,
+            'i1' : None,
+            'i2' : None,
+            'i3' : None,
+            's1' : None,
+            's2' : None,
+            's3' : None,
+            'c1' : None,
+            'c2' : None,
+            'c3' : None,
+            'disc1' : None,
+            'disc2' : None
 
-    }
+        }
 
     keys_dict = {
         '1' : {         
@@ -582,20 +572,19 @@ def grader(itemResult):
         },
     }
 
-
-
-    tally = {
-        'a' : 0,
-        'b' : 0,
-        'c':0,
-        'd':0,
-        'e':0,
-        'f':0,
-        'g':0,
-        'h':0,
-        'i':0,
-        'j':0
-    }
+    if 'tally' not in g:
+        g.tally = {
+            'a' : 0,
+            'b' : 0,
+            'c':0,
+            'd':0,
+            'e':0,
+            'f':0,
+            'g':0,
+            'h':0,
+            'i':0,
+            'j':0
+        }
     scaled_score = {}
 
     indexer = 0
@@ -606,142 +595,136 @@ def grader(itemResult):
         if subelem.attrib['identifier'] == 'SCORE' :
             for subelem2 in subelem:
                 score = int(subelem2.text)
-                g.apm_total += score
+      
         elif subelem.attrib['identifier'] == 'MAXSCORE' :
             for subelem2 in subelem:
                 max_score = int(subelem2.text)
-                g.apm_max_score += max_score
+        
         elif sub_split[0] == 'RESPONSE' :
             #print('sub_identifier',sub_identifier)
 
             try:
-                if sub_split[1] == 'DISC':
+                if sub_split[0] == 'RESPONSE':
                     #print(sub_split[0])
                     for subelem2 in subelem:
                         for subelem3 in subelem2:
+                            #print("sub elem = ")
+                            #print(subelem3.text)
                             #
                             if subelem3.text != None: #check if subelem is not None and can be strip()
                                 response = subelem3.text.strip()
-                                number = sub_split[3]
-                                indexer = int(number)
-                                print(indexer, ' : ', subelem3.text)
-                                split_response = response.split(' ')
-                                lmresponse = split_response[0]
-                                choice = split_response[1].split('_')[1]
-                                if number not in responses_array:
-                                    responses_array[number] = {}
-                                if lmresponse not in responses_array[number]:
-                                    responses_array[number][lmresponse] = {}
-                                    cursor = indexer
-                                    #print('indexer :', cursor)
-                                    pcas_aspect_letter = keys_dict[str (indexer)][choice][lmresponse]
-                                    responses_array[number][lmresponse] = pcas_aspect_letter
-                                    tally[pcas_aspect_letter] = tally[pcas_aspect_letter] + 1
+                                response_arr = response.split(' ')
+                                item_no = response_arr[0]  #RENO
+                                baris_no = response_arr[1] #RENO
+                                print("reponse : " + response)
+                                #print("item no : " + item_no[1:])
+                                #print("baris no : " + baris_no[-1:])
+                                disc_value = keys_dict[item_no[1:]][baris_no[-1:]][item_no[:1]]
+                                #print("disc value : " + disc_value)
+                                lmresponse = item_no[:1]
+                                choice = baris_no[-1:]
+                                if item_no not in g.responses_array:
+                                    g.responses_array[item_no] = {}
+                                if lmresponse not in g.responses_array[item_no]:
+                                    g.responses_array[item_no][lmresponse] = {}
+                                    g.responses_array[item_no][lmresponse] = disc_value
+                                    g.tally[disc_value] = g.tally[disc_value] + 1
                                 else:
                                     validity = False #duplicate membuat tidak valid
-                                    responses_array[number][lmresponse] = 'DUPLICATE'
-                                #print(number, lmresponse, choice )
-                                #print(responses_array)
+                                    g.responses_array[item_no][lmresponse] = 'DUPLICATE'
                                 itemGrade["candidate_response"] = response
                             else:
-                                #print('subelem3.text is None!! ', sub_split[3])
-                                number = sub_split[3]
-                                if number not in responses_array:
-                                    responses_array[number] = {}
+                                number = response_arr[0]  
+                                if number not in g.responses_array:
+                                    g.responses_array[number] = {}
             except IndexError:
                 print('[DISC] !!!ERROR!!!! ', sub_identifier)
 
-    if response == None :
-        g.apm_empty += 1
-    elif score == max_score :
-        g.apm_correct += 1
-    else :
-        g.apm_incorrect += 1
 
     data = {}
     data["type"] = "disc"
     data["scores"] = {}
-    data["scores"]["total"] = g.apm_total
-    data["scores"]["max_score"] = g.apm_max_score
-    data["answers"] = responses_array
-    data["answers"]["correct"] = tally
-    data["answers"]["incorrect"] = g.apm_incorrect
-    data["answers"]["empty"] = g.apm_empty
+
+    data["answers"] = g.responses_array
+    data["answers"]["correct"] = g.tally
+
     data["attributes"] = itemGrade
     
-    data["scores"]['disc'] = disc_score
+    data["scores"]['disc'] = g.disc_score
 
     def set_invalid(validvalue, message):
         data["scores"]["validity"] = validvalue
         print('NOT VALID!!!', message)
         return
 
-    def validate(tally):
+    def validate():
         validity = True
         tally_sum = 0
         reason = 'none'
-        for key in tally:
-            tally_sum = tally_sum + tally[key]
+        for key in g.tally:
+            tally_sum = tally_sum + g.tally[key]
+            print("tally sum : " + str(tally_sum))
         if tally_sum != 48:
             validity = False
-            print(tally_sum)
             reason = 'there are unanswered questions'
+        else :
+            validity = True
+            print("ALL 48 ITEMS ARE ANSWERED")
         #check if all question answered
         return validity, reason
     def calculate_disc(tally):
         #print(disc_score)
         
-        d1 = scale.scale('pcas-1-d', tally['a'])
-        d2 = scale.scale('pcas-2-d', tally['b'])
-        d3 = scale.scale('pcas-3-d', tally['a'] - tally['b'])
-        i1 = scale.scale('pcas-1-i', tally['c'])
-        i2 = scale.scale('pcas-2-i', tally['d'])
-        i3 = scale.scale('pcas-3-i', tally['c'] - tally['d'])
-        s1 = scale.scale('pcas-1-s', tally['e'])
-        s2 = scale.scale('pcas-2-s', tally['f'])
-        s3 = scale.scale('pcas-3-s', tally['e'] - tally['f'])
-        c1 = scale.scale('pcas-1-c', tally['g'])
-        c2 = scale.scale('pcas-2-c', tally['h'])
-        c3 = scale.scale('pcas-3-c', tally['g'] - tally['h'])
+        d1 = scale.scale('pcas-1-d', g.tally['a'])
+        d2 = scale.scale('pcas-2-d', g.tally['b'])
+        d3 = scale.scale('pcas-3-d', g.tally['a'] - g.tally['b'])
+        i1 = scale.scale('pcas-1-i', g.tally['c'])
+        i2 = scale.scale('pcas-2-i', g.tally['d'])
+        i3 = scale.scale('pcas-3-i', g.tally['c'] - g.tally['d'])
+        s1 = scale.scale('pcas-1-s', g.tally['e'])
+        s2 = scale.scale('pcas-2-s', g.tally['f'])
+        s3 = scale.scale('pcas-3-s', g.tally['e'] - g.tally['f'])
+        c1 = scale.scale('pcas-1-c', g.tally['g'])
+        c2 = scale.scale('pcas-2-c', g.tally['h'])
+        c3 = scale.scale('pcas-3-c', g.tally['g'] - g.tally['h'])
 
-        disc_score['d1'] = d1
-        disc_score['d2'] = d2
-        disc_score['d3'] = d3
-        disc_score['i1'] = i1
-        disc_score['i2'] = i2
-        disc_score['i3'] = i3
-        disc_score['s1'] = s1
-        disc_score['s2'] = s2
-        disc_score['s3'] = s3
-        disc_score['c1'] = c1
-        disc_score['c2'] = c2
-        disc_score['c3'] = c3
-        disc_score['valid_1'] = tally['i']
-        disc_score['valid_2'] = tally['j']
+        g.disc_score['d1'] = d1
+        g.disc_score['d2'] = d2
+        g.disc_score['d3'] = d3
+        g.disc_score['i1'] = i1
+        g.disc_score['i2'] = i2
+        g.disc_score['i3'] = i3
+        g.disc_score['s1'] = s1
+        g.disc_score['s2'] = s2
+        g.disc_score['s3'] = s3
+        g.disc_score['c1'] = c1
+        g.disc_score['c2'] = c2
+        g.disc_score['c3'] = c3
+        g.disc_score['valid_1'] = g.tally['i']
+        g.disc_score['valid_2'] = g.tally['j']
         #for key in tally:
         #    scaled_score[key] = scale.scale('pcas-1-c', '7')
          #   print(key)
 
-    validity = validate(tally)
+    validity = validate()
     if validity[0]: #check for validity from return value
-        calculate_disc(tally)
+        calculate_disc(g.tally)
         print('entering ....')
-        disc_graph = matching_graphs(disc_score)
-        print('before if...')
-        if disc_graph[0]:
-            data["scores"]["validity"] = True
-            data["scores"]["graph"] = disc_graph[1][0]
+        # disc_graph = matching_graphs(g.disc_score)
+        # print('before if...')
+        # if disc_graph[0]:
+        #     data["scores"]["validity"] = True
+        #     data["scores"]["graph"] = disc_graph[1][0]
 
-            print('VALID!!!')
-        else:
-            print('should set invalid')
-            set_invalid(False, disc_graph[1])
+        #     print('VALID!!!')
+        # else:
+        #     print('should set invalid')
+        #     set_invalid(False, disc_graph[1])
     else:
-        print('is is here?')
         set_invalid(False, validity[1])
 
-    print(tally)
-    print(disc_score)
+    print(g.tally)
+    print(g.disc_score)
     #print(keys[4])
     #print(tally['a'])
     return data
